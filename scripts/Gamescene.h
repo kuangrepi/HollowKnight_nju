21b1e80399f25fe7297e39f2aea6d88fd27b7cca
Fallforgotten <231880095@smail.nju.edu.cn>
2024-10-12 11:26:03 +0800

    写了大黄蜂的一些函数，但还没有把图片传进去

--- scripts/Gamescene.h
+++ scripts/Gamescene.h
@@ -8,6 +8,7 @@
 #include "Scene.h"
 #include "SceneManager.h"
 #include "Knight.h"
+#include "Hornet.h"
 
 #include <iostream>
 
@@ -15,6 +16,7 @@ extern SceneManager scene_manager;
 extern IMAGE img_gamescene_background;
 
 extern Knight* knight_1;
+extern Hornet* hornet;
 
 class Gamescene : public Scene{
     Camera camera;
@@ -25,6 +27,8 @@ public:
     void on_enter() {
         knight_1 = new Knight();
         knight_1->set_position(600, 520);
+//        hornet=new Hornet();
+//        hornet->set_position(100,520);
     }
 
     void on_exit() {
@@ -33,15 +37,18 @@ public:
 
     void on_input(const ExMessage& msg) {
         knight_1->on_input(msg);
+//        hornet->on_input(msg);
     }
 
     void on_update(int delta) {
         knight_1->on_update(delta);
+//        hornet->on_update(delta);
     }
 
     void on_draw() {
         putimage(0, 0, &img_gamescene_background);
         knight_1->on_draw(camera);
+//        hornet->on_draw(camera);
     }
 
 private:
