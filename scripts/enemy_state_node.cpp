e46fa449ee97165b9e6904dddf364daa1f7fe1d8
Fallforgotten <231880095@smail.nju.edu.cn>
2024-10-14 00:16:45 +0800

    敌人状态机的更新

--- scripts/enemy_state_node.cpp
+++ scripts/enemy_state_node.cpp
@@ -3,3 +3,37 @@
 //
 
 #include "enemy_state_node.h"
+#include "enemy.h"
+//#include "character_manager.h"
+
+EnemyAimState::EnemyAimState()
+{
+    timer.set_one_shot(true);
+    timer.set_wait_time(0.5f);
+    timer.set_on_timeout([this]()
+    {
+        Enemy* enemy = Enemy::instance()->get_enemy();
+        enemy->set_gravity_enabled(true);
+        enemy->switch_state("dash_in_air");
+    });
+}
+
+void EnemyAimState::on_enter()
+{
+    CharacterManager::instance()->get_enemy()->set_animation("aim");
+    Enemy* enemy = (Enemy*)CharacterManager::instance()->get_enemy();
+    enemy->set_gravity_enabled(false);
+    enemy->set_velocity({ 0,0 });
+    timer.restart();
+}
+
+void EnemyAimState::on_update(float delta)
+{
+    Enemy* enemy = (Enemy*)CharacterManager::instance()->get_enemy();
+    timer.on_update(delta);
+
+    if (enemy->get_hp() <= 0)
+    {
+        enemy->switch_state("dead");
+    }
+}
--- scripts/enemy_state_node.h
+++ scripts/enemy_state_node.h
@@ -6,7 +6,7 @@
 #define HOLLOWKNIGHT_DEMO_ENEMY_STATE_NODE_H
 
 #include "timer.h"
-#include "state_node.h"
+#include "statenode.h"
 
 class EnemyAimState : public StateNode
 {
@@ -30,7 +30,133 @@ public:
     void on_enter() override;
     void on_update(float delta) override;
     void on_exit() override;
+
+private:
+    const float SPEED_DASH=1200.0f;
 };
+
+class EnemyDashOnFloorState : public StateNode
+{
+public:
+    EnemyDashOnFloorState();
+    ~EnemyDashOnFloorState() = default;
+    void on_enter() override;
+    void on_update(float delta) override;
+private:
+    const float SPEED_DASH = 1000.0f;
+private:
+    Timer timer;
+};
+
+class EnemyDeadState : public StateNode
+{
+public:
+    EnemyDeadState() = default;
+    ~EnemyDeadState() = default;
+    void on_enter() override;
+};
+
+class EnemyFallState : public StateNode
+{
+public:
+    EnemyFallState() = default;
+    ~EnemyFallState() = default;
+    void on_enter() override;
+    void on_update(float delta) override;
+};
+
+class EnemyIdleState : public StateNode {
+public:
+    EnemyIdleState();
+
+    ~EnemyIdleState() = default;
+
+    void on_enter() override;
+
+    void on_update(float delta) override;
+
+    void on_exit() override;
+
+private:
+    Timer timer;
+};
+
+class EnemyJumpState : public StateNode
+{
+public:
+    EnemyJumpState() = default;
+    ~EnemyJumpState() = default;
+    void on_enter() override;
+    void on_update(float delta) override;
+private:
+    const float SPEED_JUMP = 1000.0f;
+};
+
+class EnemyRunState : public StateNode
+{
+public:
+    EnemyRunState() = default;
+    ~EnemyRunState() = default;
+
+    void on_enter() override;
+    void on_update(float delta) override;
+    void on_exit() override;
+
+private:
+    const float MIN_DIS = 350.0f;
+    const float SPEED_RUN = 500.0f;
+};
+
+class EnemySquatState : public StateNode
+{
+public:
+    EnemySquatState();
+    ~EnemySquatState() = default;
+
+    void on_enter() override;
+    void on_update(float delta) override;
+
+private:
+    Timer timer;
+};
+
+class EnemyThrowBarbState : public StateNode
+{
+public:
+    EnemyThrowBarbState();
+    ~EnemyThrowBarbState() = default;
+    void on_enter() override;
+    void on_update(float delta) override;
+private:
+    Timer timer;
+};
+class EnemyThrowSilkState : public StateNode {
+public:
+    EnemyThrowSilkState();
+
+    ~EnemyThrowSilkState() = default;
+
+    void on_enter() override;
+
+    void on_update(float delta) override;
+
+public:
+    Timer timer;
+};
+
+class EnemyThrowSwordState : public StateNode
+{
+public:
+    EnemyThrowSwordState();
+    ~EnemyThrowSwordState() = default;
+    void on_enter() override;
+    void on_update(float delta) override;
+private:
+    Timer timer_throw;
+    Timer timer_switch;
+};
+
+
 
 
 #endif //HOLLOWKNIGHT_DEMO_ENEMY_STATE_NODE_H
